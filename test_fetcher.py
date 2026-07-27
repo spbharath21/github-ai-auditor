598717ab27e83fe6b175b3d97e0de933cda32784
sagebharath <spbharath2006@gmail.com>
2026-07-27 15:39:42 +0530

    chore: remove dev artifacts and test reports from repo

--- test_fetcher.py
+++
@@ -1,9 +0,0 @@
-from auditor.fetcher import collect_repository_files
-
-files = collect_repository_files("pallets/click")
-
-print(f"Found {len(files)} code files\n")
-
-for file in files[:10]:
-  print(file.path)
-  
